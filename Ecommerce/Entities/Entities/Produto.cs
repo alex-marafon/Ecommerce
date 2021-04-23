0aa6a5e35469d091b9d4354afad4832a45b4ac38
Alex Marafon <alex.marafonn@gmail.com>
2021-04-23 17:34:38 -0400

    Base

---
+++ Ecommerce/Entities/Entities/Produto.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+   public class Produto : Notifies
+    {
+        [Column("PRD_ID")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ Ecommerce/Entities/Notifications/Notifies.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+   public class Notifies
+    {
+        public Notifies()
+        {
+            //Carregando a lista de notificaçoes ao chamar a entidade
+            Notitycoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+
+        //Metodos recebo o valor e o nome da propriedade  
+        public bool ValidarPropriedadeString(string valor, string nomepropriedade)
+        {
+            //verifica se esta vazio ou com espaço
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                //se verdade devolve a msg e retorna falso
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomepropriedade
+                });
+                return false;
+            }
+            // se tiver tudo certo retorna verdadeiro
+            return true;
+        }
+
+
+        //Metodos recebo o valor e o nome da propriedade  
+        public bool ValidarPropriedadeInt(int valor, string nomepropriedade)
+        {
+            //verifica se o valor informado é maior ou igual a 1
+            if (valor <= 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                //se verdade devolve a msg e retorna falso
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+                return false;
+            }
+            // se tiver tudo certo retorna verdadeiro
+            return true;
+        }
+
+        //Metodos recebo o valor e o nome da propriedade  
+        public bool ValidarPropriedadeDecimal(decimal valor, string nomepropriedade)
+        {
+            //verifica se o valor informado é maior ou igual a 1
+            if (valor <= 1 || string.IsNullOrWhiteSpace(nomepropriedade))
+            {
+                //se verdade devolve a msg e retorna falso
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomepropriedade
+                });
+                return false;
+            }
+            // se tiver tudo certo retorna verdadeiro
+            return true;
+        }
+    }
+}
